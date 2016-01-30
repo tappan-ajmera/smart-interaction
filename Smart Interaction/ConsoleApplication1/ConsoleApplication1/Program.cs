b68af2a8bb0bde5f5846e34261cda5f073c17a3a
tappan-ajmera <tappan.ajmera@gmail.com>
2016-01-30 21:08:33 +0530

    xmlgrammar library added
    Its just a test library for now. Useless. Adding more code to make it
    work.

---
+++ Smart Interaction/ConsoleApplication1/ConsoleApplication1/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using xmlgrammar;
+using Console = System.Console;
+
+
+namespace ConsoleApplication1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            CoreNLP cr = new CoreNLP();
+            cr.Init();
+            cr.POSTagger("Hello world");
+            Console.WriteLine("Done");
+        }
+    }
+}
